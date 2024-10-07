0f58a7c1a82773b6b66074904d50c394a1508dcc
Tumelo <tumelomarongwe@gmail.com>
2024-10-07 16:11:47 -0500

    Palindrome exercise done

--- workbook-2/PalindromeProduct/src/main/java/com/pluralsight/PalindromeProduct.java
+++ workbook-2/PalindromeProduct/src/main/java/com/pluralsight/PalindromeProduct.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class PalindromeProduct {
+    public static void main(String[] args) {
+
+        int biggestPalindrome = 0;
+        int product = 0;
+        StringBuilder productAsString;
+        for (int i = 1; i < 1000; i++){
+            for(int j=1; j<1000;j++){
+                product = i*j;
+                productAsString = new StringBuilder(String.valueOf(product));
+                boolean condition = productAsString.toString().equals(productAsString.reverse().toString());
+                if(condition){
+                    biggestPalindrome = Math.max(product,biggestPalindrome);
+                }
+            }
+        }
+        System.out.println("Biggest Palindrome: "+biggestPalindrome);
+    }
 }
