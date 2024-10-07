ec4885dee4432b5a562f18acf89d2245d626c5c6
Tumelo <tumelomarongwe@gmail.com>
2024-10-07 16:08:57 -0500

    Added the Bedtime Stories exercise

---
+++ workbook-3/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
@@ -0,0 +1,70 @@
+package com.pluralsight;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class BedtimeStories {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Select an option by typing the respective number\n" +
+                "1.Goldilocks\n" +
+                "2.Hansel and Gretel\n" +
+                "3.Mary had a little lamb\n");
+        int choice = userInput.nextInt();
+        if (!(choice<4 && choice>0))
+            System.out.println("Wrong input");
+        else
+            switch(choice){
+
+                case 1:
+                    try {
+                        FileInputStream goldilocks = new FileInputStream("src/resources/goldilocks.txt");
+                        Scanner fileInput = new Scanner(goldilocks);
+                        String line;
+                        System.out.println("File path"+new File(".").getAbsolutePath());
+                        int i = 1;
+                        while(fileInput.hasNextLine()){
+                            line = fileInput.nextLine();
+                            System.out.println(i++ +"."+line);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    try {
+                        FileInputStream goldilocks = new FileInputStream("src/resources/hansel_and_gretel.txt");
+                        Scanner fileInput = new Scanner(goldilocks);
+                        String line;
+                        System.out.println("File path"+new File(".").getAbsolutePath());
+                        int i = 1;
+                        while(fileInput.hasNextLine()){
+                            line = fileInput.nextLine();
+                            System.out.println(i++ +"."+line);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case 3:
+                    try {
+                        FileInputStream goldilocks = new FileInputStream("src/resources/mary_had_a_little_lamb.txt");
+                        Scanner fileInput = new Scanner(goldilocks);
+                        String line;
+                        System.out.println("File path"+new File(".").getAbsolutePath());
+                        int i = 1;
+                        while(fileInput.hasNextLine()){
+                            line = fileInput.nextLine();
+                            System.out.println(i++ +"."+line);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+            }
+
+    }
+}
