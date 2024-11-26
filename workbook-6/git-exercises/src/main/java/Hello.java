7127c371dc9c69f68ec5731dda05a8ac9268db2c
Tumelo <tumelomarongwe@gmail.com>
2024-11-26 09:30:44 -0600

    Added a branch for the customizable greeting feature

--- workbook-6/git-exercises/src/main/java/Hello.java
+++ workbook-6/git-exercises/src/main/java/Hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input  = new Scanner(System.in);
+        System.out.println("What is your name");
+        String name = input.nextLine().trim();
+        System.out.printf("Hello %s!",name);
     }
 }
