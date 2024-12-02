1685ef6280995e8dd36e8bc27aec4d51dfc36386
Tumelo <tumelomarongwe@gmail.com>
2024-12-02 15:23:54 -0600

    Week 10 Day 1. Second exercise.

--- workbook-8/NorthWindSuppliers/src/main/java/com/pluralsight/Northwind_Exercise2.java
+++ workbook-8/NorthWindSuppliers/src/main/java/com/pluralsight/Northwind_Exercise2.java
@@ -1,21 +1,22 @@
 package com.pluralsight;
 import java.sql.*;
 
-public class Northwind {
+public class Northwind_Exercise2 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String userName = args[0];
+        String password = args[1];
 
         Connection connection;
         connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "yearup");
+                userName,
+                password);
 
-        Statement statement = connection.createStatement();
-        String query = "SELECT ProductName, CategoryName FROM northwind.products p " +
-                "JOIN northwind.categories c on  p.CategoryID = c.CategoryID";
+        PreparedStatement statement = connection.prepareStatement("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM northwind.products");
 
-        ResultSet results = statement.executeQuery(query);
+        ResultSet results = statement.executeQuery();
 
         for (int i = 1; i<= results.getMetaData().getColumnCount(); i++) {
             System.out.print(results.getMetaData().getColumnLabel(i)+ "|");
@@ -27,6 +28,8 @@ public class Northwind {
             }
             System.out.println();
         }
+        results.close();
+        statement.close();
         connection.close();
 
     }
