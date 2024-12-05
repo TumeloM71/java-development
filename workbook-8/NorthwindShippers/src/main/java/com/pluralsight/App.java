5bced19488ababeb0d4fa151da846fb9f8da1717
Tumelo <tumelomarongwe@gmail.com>
2024-12-05 16:20:10 -0600

    Week 10 Day 4. End of day commit

--- workbook-8/NorthwindShippers/src/main/java/com/pluralsight/App.java
+++ workbook-8/NorthwindShippers/src/main/java/com/pluralsight/App.java
@@ -1,4 +1,68 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.util.Scanner;
+
 public class App {
+
+    static Scanner scanner ;
+    public static void main(String[] args) {
+
+        String userName = args[0];
+        String password = args[1];
+        scanner = new Scanner(System.in);
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        DataManager.setDataSource(dataSource);
+        DataManager.setScanner(scanner);
+
+        boolean loopFlag = true;
+
+        try {
+            while (loopFlag) {
+                System.out.println();
+                System.out.print("What do you want to do?\n" +
+                        "1) Add a new shipper\n" +
+                        "2) Display shippers\n " +
+                        "0) Exit\n" +
+                        "Select an option: ");
+
+                int selection = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println();
+                switch (selection) {
+                    case 1 -> addNewShipper();
+                    case 2 -> displayShippers();
+                    case 0 -> loopFlag = false;
+                    default -> System.out.println("Invalid input. Try again");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    public static void displayShippers(){
+        DataManager.displayShippers();
+    }
+
+    public static void addNewShipper(){
+        DataManager.addShipper(askForCompanyName(), askForPhoneNumber());
+    }
+
+    public static String askForCompanyName(){
+        System.out.println("What is the company name ?");
+        return scanner.nextLine().trim();
+    }
+
+    public static String askForPhoneNumber(){
+        System.out.println("What it the phone number ?");
+        return scanner.nextLine().trim();
+    }
 }
--- workbook-8/NorthwindShippers/src/main/java/com/pluralsight/DataManager.java
+++ workbook-8/NorthwindShippers/src/main/java/com/pluralsight/DataManager.java
@@ -1,4 +1,118 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.*;
+import java.util.Scanner;
+
 public class DataManager {
+
+    static String userName = null;
+    static String password = null;
+    static Scanner scanner = new Scanner(System.in);
+    static BasicDataSource dataSource;
+
+    public static void addShipper(String companyName, String phone){
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO northwind.shippers (CompanyName, Phone) " +
+                    "VALUES (?, ?)",Statement.RETURN_GENERATED_KEYS)){
+            statement.setString(1,companyName);
+            statement.setString(2,phone);
+
+            int rows = statement.executeUpdate();
+            System.out.println("Rows updated: "+rows);
+            displayAddedKeys(statement);
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void displayShippers(String column, String attribute){
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM northwind.shippers WHERE ? LIKE ?"))
+        {
+            statement.setString(1,column);
+            statement.setString(2,column);
+            displayResults(statement);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void displayShippers(){
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM northwind.shippers"))
+        {
+            displayResults(statement);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void displayAddedKeys(PreparedStatement statement){
+
+        try (ResultSet keys = statement.getGeneratedKeys()
+        ) {
+            while (keys.next()) {
+                System.out.printf("%d key was added\n",
+                        keys.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void displayResults(PreparedStatement statement) {
+        try (ResultSet results = statement.executeQuery()) {
+            while (results.next()) {
+                for (int i = 1; i <= results.getMetaData().getColumnCount(); i++) {
+                    String columnLabel = results.getMetaData().getColumnLabel(i);
+                    String columnVal = results.getString(i);
+                    String spaces = " ".repeat(15-columnLabel.length());
+                    System.out.print(columnLabel+spaces+": ");
+                    System.out.print(columnVal + '\n');
+                }
+                System.out.println("---".repeat(80));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        DataManager.userName = userName;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        DataManager.password = password;
+    }
+
+    public static Scanner getScanner() {
+        return scanner;
+    }
+
+    public static void setScanner(Scanner scanner) {
+        DataManager.scanner = scanner;
+    }
+
+    public static BasicDataSource getDataSource() {
+        return dataSource;
+    }
+
+    public static void setDataSource(BasicDataSource dataSource) {
+        DataManager.dataSource = dataSource;
+    }
 }
